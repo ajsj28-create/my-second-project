53e7e910864fb4a7345a8593a7fb27bb320121c9
ajsj.28 <ajsj.28@gmail.com>
2025-12-07 18:19:33 +0530

    signup form added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InfoComponent } from './shared/components/info/info.component';
 import { TruncatePipe } from './shared/pipes/truncate.pipe';
+import { SignUpformComponent } from './shared/components/sign-upform/sign-upform.component';
+import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     InfoComponent,
-    TruncatePipe
+    TruncatePipe,
+    SignUpformComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/components/sign-upform/sign-upform.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-upform',
+  templateUrl: './sign-upform.component.html',
+  styleUrls: ['./sign-upform.component.scss']
+})
+export class SignUpformComponent implements OnInit {
+
+  @ViewChild('signUpform') form!: NgForm;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSignUp() {
+    console.log(this.form);
+  }
+}
